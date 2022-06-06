37f391c118bf04c0ce0102abfccd05701fbd306f
= <meomancer@gmail.com>
2022-06-06 10:53:59 +0700

    Fix layout on firefox

--- django_project/frontend/src/components/Navbar/index.jsx
+++ django_project/frontend/src/components/Navbar/index.jsx
@@ -3,6 +3,7 @@
    ========================================================================== */
 
 import React from 'react';
+import $ from 'jquery';
 import i18n from "i18next";
 import Button from "@mui/material/Button";
 
@@ -16,36 +17,43 @@ import './style.scss';
  * **/
 export default function NavBar() {
   const { icon, site_title } = preferences;
+
+  // Set width of logo
+  // Not working using css on firefox
+  $('.page__header-logo').width($('.page__header-link').width());
+
   return (
-    <header className='page__header'>
-      <ul className='page__header-menu'>
-        <li className='page__header-logo'>
-          <a
-            href='/'
-            title={i18n.t('Homepage')}
-            className='page__header-link'
-          >
-            <img src={icon} alt="Logo"/>
-          </a>
-        </li>
-        <li className='page__header-title'>
-          <Button>
+    <header>
+      <div className='page__header'>
+        <ul className='page__header-menu'>
+          <li className='page__header-logo'>
             <a
               href='/'
               title={i18n.t('Homepage')}
               className='page__header-link'
             >
-              {site_title}
+              <img src={icon} alt="Logo"/>
             </a>
-          </Button>
-        </li>
-        <li>
-          <Links/>
-        </li>
-        <li>
-          <User/>
-        </li>
-      </ul>
+          </li>
+          <li className='page__header-title'>
+            <Button>
+              <a
+                href='/'
+                title={i18n.t('Homepage')}
+                className='page__header-link'
+              >
+                {site_title}
+              </a>
+            </Button>
+          </li>
+          <li>
+            <Links/>
+          </li>
+          <li>
+            <User/>
+          </li>
+        </ul>
+      </div>
     </header>
   )
 }
