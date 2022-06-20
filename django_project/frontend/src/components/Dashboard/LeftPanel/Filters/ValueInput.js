b2eadcc3ecfabfad5e6252e60c374e68559a6d8c
= <meomancer@gmail.com>
2022-06-20 15:26:42 +0700

    Update Select of as autocomplete

--- django_project/frontend/src/components/Dashboard/LeftPanel/Filters/ValueInput.js
+++ django_project/frontend/src/components/Dashboard/LeftPanel/Filters/ValueInput.js
@@ -1,55 +1,29 @@
+/* ==========================================================================
+   Value input for filters
+   ========================================================================== */
 import React, { Fragment } from "react";
+import { Input, } from "@mui/material";
 import {
-  Checkbox,
-  Input,
-  ListItemText,
-  OutlinedInput,
-  Select
-} from "@mui/material";
-import MenuItem from "@mui/material/MenuItem";
+  MultipleSelectWithSearch,
+  SelectWithSearch
+} from "../../../Input/SelectWithSearch";
 
 export default function FilterValueInput(
   { operator, value, indicator, onChange }
 ) {
   return <Fragment>{
     operator === 'IN' ?
-      <Select
-        className='FilterInput'
-        multiple
-        value={value}
-        onChange={(event) => {
-          onChange(event.target.value);
-        }
-        }
-        input={<OutlinedInput label="Tag"/>}
-        renderValue={(selected) => selected.length + ' selected'}
-      >
-        {indicator ? indicator.data.map((name) => (
-          <MenuItem key={name} value={name}>
-            <Checkbox checked={value.indexOf(name) > -1}/>
-            <ListItemText primary={name}/>
-          </MenuItem>
-        )) : ''}
-      </Select> :
+      (
+        indicator ? <MultipleSelectWithSearch
+          value={value} onChangeFn={onChange}
+          options={indicator.data} className='FilterInput'/> : ''
+      ) :
       (
         operator === '=' && indicator && isNaN(indicator.data[0]) ?
-          <Select
-            className='FilterInput'
-            value={value}
-            onChange={
-              (event) => {
-                onChange(event.target.value);
-              }
-            }
-          >
-            {indicator ? indicator.data.map((name) => (
-              <MenuItem key={name} value={name}>
-                <ListItemText primary={name}/>
-              </MenuItem>
-            )) : ''}
-          </Select> :
+          <SelectWithSearch
+            value={value} onChangeFn={onChange}
+            options={indicator.data} className='FilterInput'/> :
           <Input
-            key="input1"
             className='FilterInput'
             type="text"
             placeholder="Value"
